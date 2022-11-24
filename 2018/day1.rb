ed133602d65ffe23b3aa33062ff061efe3ee4e65
Daniel Grieve <dnlgrv@hey.com>
2022-11-24 13:42:18 +0000

    Complete 2018/day1 part2

--- 2018/day1.rb
+++ 2018/day1.rb
@@ -8,6 +8,17 @@ class Day1 < Advent::Solution
   end
 
   def part2(input: load_input)
+    frequencies = [0]
+
+    prepare(input).cycle.each do |adjustment|
+      frequency = frequencies.last + adjustment
+
+      if frequencies.include? frequency
+        break frequency
+      else
+        frequencies << frequency
+      end
+    end
   end
 
   private
--- 2018/test/day1_test.rb
+++ 2018/test/day1_test.rb
@@ -16,7 +16,10 @@ class Day1Test < Advent::TestCase
     assert_equal(-6, @solution.part1(input: "-2\n-2\n-2"))
   end
 
-  # def test_part2
-  #   assert_equal 123, @solution.part2
-  # end
+  def test_part2
+    assert_equal 0, @solution.part2(input: "+1\n-1")
+    assert_equal 10, @solution.part2(input: "+3\n+3\n+4\n-2\n-4")
+    assert_equal 5, @solution.part2(input: "-6\n+3\n+8\n+5\n-6")
+    assert_equal 14, @solution.part2(input: "+7\n+7\n-2\n-7\n-4")
+  end
 end

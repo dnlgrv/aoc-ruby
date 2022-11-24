914d4389adacf21f35dd0158a0b6663fa1a63173
Daniel Grieve <dnlgrv@hey.com>
2022-11-24 13:33:39 +0000

    Complete 2018/day1 part1

---
+++ 2018/test/day1_test.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "advent"
+require "minitest/autorun"
+
+require_relative "../day1"
+
+class Day1Test < Advent::TestCase
+  def setup
+    @solution = Day1.new
+  end
+
+  def test_part1
+    assert_equal 3, @solution.part1(input: "+1\n+1\n+1")
+    assert_equal 0, @solution.part1(input: "+1\n+1\n-2")
+    assert_equal(-6, @solution.part1(input: "-2\n-2\n-2"))
+  end
+
+  # def test_part2
+  #   assert_equal 123, @solution.part2
+  # end
+end

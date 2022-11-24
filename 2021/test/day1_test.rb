dbeb38f3c80b24058de9a3a89e6719c1b2c22d42
Daniel Grieve <dnlgrv@hey.com>
2022-11-24 20:31:02 +0000

    Complete 2021/day1 part1

---
+++ 2021/test/day1_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "advent"
+require "minitest/autorun"
+
+require_relative "../day1"
+
+class Day1Test < Advent::TestCase
+  def setup
+    @solution = Day1.new
+    @input = %(
+      199
+      200
+      208
+      210
+      200
+      207
+      240
+      269
+      260
+      263
+    )
+  end
+
+  def test_part1
+    assert_equal 7, @solution.part1(input: @input)
+  end
+
+  # def test_part2
+  #   assert_equal 123, @solution.part2
+  # end
+end

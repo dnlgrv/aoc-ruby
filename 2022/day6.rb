16d8b6a0d7196691539528b5919df2057e383a9b
Daniel Grieve <dnlgrv@hey.com>
2022-12-06 09:06:27 +0000

    Refactor 2022/day6

--- 2022/day6.rb
+++ 2022/day6.rb
@@ -4,24 +4,22 @@ require "advent"
 
 class Day6 < Advent::Solution
   def part1(input: load_input)
-    prepare(input).each_cons(4).with_index do |quad, index|
-      if quad.uniq.size == 4
-        return index + 1 + 3 # index offset + consecutive offset
-      end
-    end
+    find_consecutive_unique_index(prepare(input), 4)
   end
 
   def part2(input: load_input)
-    prepare(input).each_cons(14).with_index do |quad, index|
-      if quad.uniq.size == 14
-        return index + 1 + 13 # index offset + consecutive offset
-      end
-    end
+    find_consecutive_unique_index(prepare(input), 14)
   end
 
   private
 
   def prepare(input)
     input.chars
+  end
+
+  def find_consecutive_unique_index(input, count)
+    input.each_cons(count).with_index.find_index do |arr, index|
+      arr.uniq.size == count
+    end + count
   end
 end

728ab99e695858c7c76e0614b85b3698a04add8a
Daniel Grieve <dnlgrv@hey.com>
2023-12-03 21:08:46 +0000

    Day 3 part 2

--- 2023/day3.rb
+++ 2023/day3.rb
@@ -1,4 +1,5 @@
 require "advent"
+require "debug"
 
 class Day3 < Advent::Solution
   def part_1(input: input_text)
@@ -49,5 +50,40 @@ class Day3 < Advent::Solution
   end
 
   def part_2(input: input_text)
+    line_length = input.lines.first.length - 1
+
+    numbers = {}
+
+    input.gsub(/\n/, "").scan(/[0-9]+/) do |match|
+      range_start, range_end = $~.offset(0)
+      numbers[(range_start...range_end)] = match
+    end
+
+    ratios = []
+
+    input.gsub(/\n/, "").scan /\*/ do
+      index = $~.offset(0).first
+
+      above = index - line_length
+      above_right = index - line_length + 1
+      right = index + 1
+      below_right = index + line_length + 1
+      below = index + line_length
+      below_left = index + line_length - 1
+      left = index - 1
+      above_left = index - line_length - 1
+
+      gears = [above, above_right, right, below_right, below, below_left, left, above_left].inject([]) do |matching, around_index|
+        if gear = numbers.select { _1 === around_index }
+          matching << gear 
+        end
+      end.select { _1.any? }.uniq { _1.keys.first }.flat_map { _1.values }
+
+      if gears.size == 2
+        ratios << gears.map(&:to_i).inject(:*)
+      end
+    end
+
+    ratios.sum
   end
 end

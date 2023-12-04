ff604df989b3aed2b2c0a81cd33606d6d10b9b5a
Daniel Grieve <dnlgrv@hey.com>
2023-12-04 09:01:09 +0000

    Only read data once

--- test/2023/day3_test.rb
+++ test/2023/day3_test.rb
@@ -3,17 +3,19 @@ require "minitest/autorun"
 
 require_relative "../../2023/day3"
 
+SAMPLE_INPUT = DATA.read.chomp
+
 class Day3Test < Advent::TestCase
   def setup
     @solution = Day3.new
   end
 
   def test_part_1
-    assert_equal 4361, @solution.part_1(input: DATA.read.chomp)
+    assert_equal 4361, @solution.part_1(input: SAMPLE_INPUT)
   end
 
   def test_part_2
-    assert_equal 467835, @solution.part_2(input: DATA.read.chomp)
+    assert_equal 467835, @solution.part_2(input: SAMPLE_INPUT)
   end
 end
 
